176b3ab5f34d2148917ea7ca80f7408292c6a8c0
Tejas Vasishta G R <wasishta38@gmail.com>
2026-08-23 07:41:26 +0530

    Created Project

---
+++ vault_nest/src/main/java/com/example/vault_nest/VaultNestApplication.java
@@ -0,0 +1,13 @@
+package com.example.vault_nest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaultNestApplication.class, args);
+	}
+
+}
---
+++ vault_nest/src/test/java/com/example/vault_nest/VaultNestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vault_nest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaultNestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
