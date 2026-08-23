ab1ab8c6a7c872721a3ca06dd21a4510a107af22
GR-tejas <95667323+GR-tejas@users.noreply.github.com>
2026-08-23 23:37:33 +0530

    temp webpage

---
+++ vault_nest/src/main/resources/static/index.js
@@ -0,0 +1,10 @@
+const uploadBtn = $("#uploadButton");
+
+uploadBtn.click(function {
+    const fileInput = $("fileInput");
+    const file = fileInput[0];
+    console.log(file);
+    console.log(file.name);
+    console.log(file.size);
+    console.log(file.type);
+})
