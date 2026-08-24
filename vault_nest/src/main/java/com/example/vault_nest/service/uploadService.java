094bdc597a5e98dbb49fc15b0f4fe563138675a3
Tejas Vasishta G R <wasishta38@gmail.com>
2026-08-24 23:10:05 +0530

    Functional file storage system

--- vault_nest/src/main/java/com/example/vault_nest/service/uploadService.java
+++ vault_nest/src/main/java/com/example/vault_nest/service/uploadService.java
@@ -1,19 +1,30 @@
 package com.example.vault_nest.service;
 
-import java.io.File;
+//import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class uploadService {
-    //String uploadDirectory = "G:\\Volume H stuff\\LocalCloudStorage";
-    String uploadDirectory = "C:\\Users\\wasis\\Desktop\\LocalCloudProject\\CloudStorage";
+
+    String uploadDirectory = "G:\\Volume H stuff\\LocalCloudStorage";
+    //String uploadDirectory = "C:\\Users\\wasis\\Desktop\\LocalCloudProject\\CloudStorage";
+
     public String uploadFile(MultipartFile file) {
         
         try {
-            file.transferTo(new File(uploadDirectory + file.getOriginalFilename()));
+
+            Path userDirectory = Paths.get(uploadDirectory, "user1");
+
+            Files.createDirectories(userDirectory);
+
+            Path filePath = Paths.get(userDirectory.toString(), file.getOriginalFilename());
+            file.transferTo(filePath);
 
             return "File uploaded successfully: " + file.getOriginalFilename();
 
