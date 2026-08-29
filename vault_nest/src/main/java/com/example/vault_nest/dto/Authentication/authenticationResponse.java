f4a45118779870fd7b1486512ff823c2b67f66af
Tejas Vasishta G R <wasishta38@gmail.com>
2026-08-29 20:44:30 +0530

    Update authenticationResponse.java

--- vault_nest/src/main/java/com/example/vault_nest/dto/Authentication/authenticationResponse.java
+++ vault_nest/src/main/java/com/example/vault_nest/dto/Authentication/authenticationResponse.java
@@ -1,5 +1,33 @@
 package com.example.vault_nest.dto.Authentication;
 
 public class authenticationResponse {
+    private String username;
+    private String password;
 
+    // Default constructor
+    public authenticationResponse() {
+    }
+
+    // Parameterized constructor
+    public authenticationResponse(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    // Getters and Setters
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
